a8fa19f3d84549f601d40b41ea3fec39b8734d2a
Maryna Markova <markova.maryna@gmail.com>
2023-05-05 23:56:38 +0300

    create form

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Color Palette Project using OpenAI
+# Color Palette Project
+
+using OpenAI is built on [Mastering OpenAI Python APIs: Unleash the Power of GPT4](https://www.udemy.com/course/mastering-openai) Udemy course with [Colt Steele](https://www.udemy.com/course/mastering-openai/#instructor-1)
+
+- built in Python3
+- uses OpenAI
+- uses Flask
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import openai
 from flask import Flask, render_template, request
 from dotenv import dotenv_values
+import json
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
@@ -8,20 +9,46 @@ openai.api_key = config["OPENAI_API_KEY"]
 app = Flask(__name__, template_folder="templates")
 
 
-# @app.route("/palette", methods=["POST"])
-# def prompt_to_palette():
-#    # OPEN AI COMPLETEION CALL
+def get_colors(msg):
+    prompt = f"""
+    You are a color palette generating assistant that responds to text prompts for color palettes.
 
-#    # RETURN LIST OF COLORS
+    You should generate color palettes that fit the theme, mood or instructions in the prompt.
+    
+    The palette should be between 2 and 8 colors.
+
+    Q: Convert the following verbal description of a color palette into a list of colors: The Mediterreanean Sea
+    A: ["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]
+
+    Q: Convert the following verbal description of a color palette into a list of colors: sage, nature, earth
+    A: ["#EDF1D6", "#9DC08B", "#609966", "#40513B"]
+
+    Desired Format: a JSON array of hexadeciaml color codes
+
+    Q: Convert the following verbal description of a color palette into a list of colors: {msg}
+    A: 
+    """
+
+    response = openai.Completion.create(
+        prompt=prompt,
+        model="text-davinci-003",
+        max_tokens=200,
+    )
+
+    colors = json.loads(response["choices"][0]["text"])
+    return colors
+
+
+@app.route("/palette", methods=["POST"])
+def prompt_to_palette():
+    query = request.form.get("query")
+    colors = get_colors(query)
+    return {"colors": colors}
 
 
 @app.route("/")
 def index():
-    response = openai.Completion.create(
-        model="text-davinci-003", prompt="How to translate Snorfle in ukrainian: "
-    )
-    return response["choices"][0]["text"]
-    # return render_template("index.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
